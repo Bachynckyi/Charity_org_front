1cf64542bcc26b0a39fecc07a9e69da8b4a02294
Andrew <93881817+Bachynckyi@users.noreply.github.com>
2024-03-21 23:15:11 +0200

    basic_7

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -21,46 +21,51 @@ const Header = () => {
           </NavLink>
         </div>
         <div className={scss.links_social}>
-          <NavLink>
-            <img src={instagram_logo} alt='instagram_logo' className={scss.social_logos}/>
+          <NavLink className={scss.social_logos}>
+            <img src={instagram_logo} alt='instagram_logo' />
           </NavLink>
-          <NavLink>
+          <NavLink className={scss.social_logos}>
             <img src={facebook_logo} alt='facebook_logo'/>
           </NavLink>
-          <NavLink>
+          <NavLink className={scss.social_logos}>
             <img src={linkedin_logo} alt='linkedin_logo'/>
           </NavLink>
         </div>
       </div>
       <div className={scss.container_navigation}>
           <Logo/>
-          <div className={scss.navigation}>
-              <NavLink to="/about" className={scss.navigation_item}>
-                  ПРО ФОНД
-              </NavLink>
-              <NavLink to="/" className={scss.navigation_item}>
-                  ПРОЄКТИ
-              </NavLink>
-              <NavLink to="/" className={scss.navigation_item}>
-                  НОВИНИ
-              </NavLink>
-              <NavLink to="/contacts" className={scss.navigation_item}>
-                  КОНТАКТИ
-              </NavLink>
-              <NavLink to="/" className={scss.navigation_item}>
-                  ДОКУМЕНТИ
-              </NavLink>
-              <NavLink to="/details" className={scss.navigation_item}>
-                  РЕКВІЗИТИ
-              </NavLink>
-          </div>
-          <div className={scss.language_container}>
-              <NavLink to="/" className={scss.language_item_current}>
-                  UA
-              </NavLink>
-              <NavLink to="/" className={scss.language_item}>
-                  EN
-              </NavLink>
+          <div className={scss.wrapper}>
+            <div className={scss.navigation}>
+                <NavLink to="/" className={({isActive}) => isActive ? scss.navigation_item_active : scss.navigation_item}>
+                    ГОЛОВНА
+                </NavLink>
+                <NavLink to="/about" className={({isActive}) => isActive ? scss.navigation_item_active : scss.navigation_item}>
+                    ПРО ФОНД
+                </NavLink>
+                <NavLink to="/projects" className={({isActive}) => isActive ? scss.navigation_item_active : scss.navigation_item}>
+                    ПРОЄКТИ
+                </NavLink>
+                <NavLink to="/news" className={({isActive}) => isActive ? scss.navigation_item_active : scss.navigation_item}>
+                    НОВИНИ
+                </NavLink>
+                <NavLink to="/contacts" className={({isActive}) => isActive ? scss.navigation_item_active : scss.navigation_item}>
+                    КОНТАКТИ
+                </NavLink>
+                <NavLink to="documents" className={({isActive}) => isActive ? scss.navigation_item_active : scss.navigation_item}>
+                    ДОКУМЕНТИ
+                </NavLink>
+                <NavLink to="/details" className={({isActive}) => isActive ? scss.navigation_item_active : scss.navigation_item}>
+                    РЕКВІЗИТИ
+                </NavLink>
+            </div>
+            <div className={scss.language_container}>
+                  <NavLink to="/" className={scss.language_item_current}>
+                      UA
+                  </NavLink>
+                  <NavLink to="/" className={scss.language_item}>
+                      EN
+                  </NavLink>
+            </div>
           </div>
       </div>
     </div>
--- src/pages/DetailsPage/DetailsPage.jsx
+++ src/pages/DetailsPage/DetailsPage.jsx
@@ -8,17 +8,81 @@ const DetailsPage = () => {
         <div className={scss.title_container}>
             <span className={scss.title}>Реквізити</span>
         </div>
-        <div className={scss.content_container}>
-            <div className={scss.contacts_container}>
-                <p className={scss.contact_name_first}>Юридична адреса:</p>
-                <address className={scss.address}>Україна, 09801, Київська обл., Білоцерківський р-н, місто Тетіїв, вул.Центральна, будинок 105А</address>
-                <p className={scss.contact_name}>Електронна пошта:</p>
-                <a className={scss.contact_link} href='emailto: unityhorizon@gmail.com'>unityhorizon@gmail.com</a>
-                <p className={scss.contact_name}>Телефон:</p>
-                <a className={scss.contact_link} href='tel: +38093703808'>+38 (093) 703 80 08</a>
-                <p className={scss.contact_name}>Ідентифікаційний код юридичної особи:</p>
-                <p className={scss.contact_link}>45502942</p>
+        <div className={scss.details_container}>
+            <span className={scss.payment_details}>Для оплати за реквізитами:</span>
+            <div className={scss.input_container}>
+              <label className={scss.input_option} htmlFor="UAH"> 
+                  <input
+                      className={scss.radio_button}
+                      type="radio"
+                      id="UAH"
+                      name="currency"
+                      value="UAH"
+                  />
+                  <span className={scss.custom_button}></span>
+                  <span className={scss.input_name}>UAH</span>
+              </label>
+              <label className={scss.input_option} htmlFor="USD"> 
+                  <input
+                      className={scss.radio_button}
+                      type="radio"
+                      id="USD"
+                      name="currency"
+                      value="USD"
+                  />
+                  <span className={scss.custom_button}></span>
+                  <span className={scss.input_name}>USD</span>
+              </label>
+              <label className={scss.input_option} htmlFor="EUR"> 
+                  <input
+                      className={scss.radio_button}
+                      type="radio"
+                      id="EUR"
+                      name="currency"
+                      value="EUR"
+                  />
+                  <span className={scss.custom_button}></span>
+                  <span className={scss.input_name}>EUR</span>
+              </label>
+              <label className={scss.input_option} htmlFor="GBP"> 
+                  <input
+                      className={scss.radio_button}
+                      type="radio"
+                      id="GBP"
+                      name="currency"
+                      value="GBP"
+                  />
+                  <span className={scss.custom_button}></span>
+                  <span className={scss.input_name}>GBP</span>
+              </label>
+              <label className={scss.input_option} htmlFor="PLN"> 
+                  <input
+                      className={scss.radio_button}
+                      type="radio"
+                      id="PLN"
+                      name="currency"
+                      value="PLN"
+                  />
+                  <span className={scss.custom_button}></span>
+                  <span className={scss.input_name}>PLN</span>
+              </label>
+              <label className={scss.input_option} htmlFor="CAD"> 
+                  <input
+                      className={scss.radio_button}
+                      type="radio"
+                      id="CAD"
+                      name="currency"
+                      value="CAD"
+                  />
+                  <span className={scss.custom_button}></span>
+                  <span className={scss.input_name}>CAD</span>
+              </label>
             </div>
+            <div className={scss.name_container}>
+              <span className={scss.name_company}>Найменування українською мовою (для переказів всередині країни) — БФ «МІЖНАРОДНИЙ БЛАГОДІЙНИЙ ФОНД ГОРИЗОНТ ЄДНОСТІ»</span>
+              <span className={scss.name_company}>Name of Company for transfers from abroad – CHARITABLE FOUNDATION «INTERNATIONAL CHARITABLE FOUNDATION UNITY HORIZON»</span>
+            </div>
+            <span className={scss.name_text}>ДЯКУЄМО КОЖНОМУ НЕБАЙДУЖОМУ!</span>
         </div>
     </div>
   );
