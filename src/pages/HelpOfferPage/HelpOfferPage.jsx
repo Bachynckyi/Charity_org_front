fe29254f0479d61a232e662c6d5539908ad70712
Andrew <93881817+Bachynckyi@users.noreply.github.com>
2024-05-05 09:39:38 +0300

    update_05/05/2024

--- src/pages/HelpOfferPage/HelpOfferPage.jsx
+++ src/pages/HelpOfferPage/HelpOfferPage.jsx
@@ -18,7 +18,7 @@ const HelpOfferPage = () => {
             <div className={scss.title_container}>
                 <h1 className={scss.title}>Надати допомогу</h1>
             </div>
-            <form className={scss.wrapper}>
+            <div className={scss.wrapper}>
                 <span className={scss.form_title}>Оберіть вид допомоги</span>
                 <div className={scss.form_selector}>
                         <label className={scss.input_option} htmlFor="payment"> 
@@ -63,7 +63,7 @@ const HelpOfferPage = () => {
                 {typeHelp === "payment" && (<BankDetails/>)}
                 {typeHelp === "becomePartner" && (<OfferPartner/>)}
                 {typeHelp === "otherHelp" && (<ContactForm/>)}
-            </form>
+            </div>
 
         </div>
     );
